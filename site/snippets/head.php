caa466c78754a506aa3ae35fd3610a2a49d1c138
chelozuber <108348447+chelozuber@users.noreply.github.com>
2024-10-30 19:43:46 -0300

    Agregado de Google Analytics

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3MQ860DV50"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-3MQ860DV50');
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow">
@@ -11,23 +20,18 @@
     <meta name="url" content="<?= $site->url() ?>">
     <meta property="og:url" content="<?= $site->url() ?>">
     <meta property="og:locale" content="en_US">
-
     <?php snippet('head_seo') ?>
-
     <!-- FAVICON -->
-
     <?= css([
         'assets/css/normalize.css',
         'assets/css/typography.css',
         'assets/css/elements.css',
         'assets/css/styles.css',
         'assets/css/grid.css',
-
         'assets/css/snippets/header.css',
         'assets/css/templates/' . $page->template()->name() . '.css',
         'assets/css/snippets/footer.css',
     ]) ?>
-
     <?= js([
         'assets/js/main.js',
     ], ['defer' => true]) ?>
