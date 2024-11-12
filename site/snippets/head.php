3bf1661023e61dc73dbf4672253e37cefbad9dbd
chelozuber <108348447+chelozuber@users.noreply.github.com>
2024-11-12 17:11:18 -0300

    Update head.php

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow">
-    <meta name="author" content="Pedro Zuber">
+    <meta name="author" content="Pedro Meireis">
     <meta name="email" content="hello@meireis.com">
     <meta name="copyright" content="<?= $site->copyright() ?>">
     <meta property="og:type" content="website">
